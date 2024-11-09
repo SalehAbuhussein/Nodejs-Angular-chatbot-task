7a4db774af2813c0331e228e06493545db8a7c2f
Saleh <salehabuhussein2@gmail.com>
2024-11-09 18:00:30 +0300

    Remove .env from gitignore

--- chatbot-demo-api/src/routes/chatbotRoutes.ts
+++ chatbot-demo-api/src/routes/chatbotRoutes.ts
@@ -4,8 +4,6 @@ import * as chatbotController from '../controllers/chatbotController';
 
 const router = Router();
 
-router.get('/image', chatbotController.getChatbotImageResponse);
 router.post('/text', chatbotController.postChatbotTextResponse);
-// router.get('/image');
 
 export default router;
